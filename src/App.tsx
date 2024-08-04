ef89d036fa804fd83766fab0cb48b1e6092c7fde
sunghyun_son <sunghyun_son@tmax.co.kr>
2024-08-04 14:47:52 +0900

    [feat] emotion 세팅
    css-in js을 지원해주는 패키지로
    1. 컴포넌트간 스타일 충돌 방지
    2. 자바스크립트 변수로 사용할 수 있어 스타일을 동적으로 변경할 수 있음
    3. 사용되는 스타일만 번들에 포함하여 성능상 이점이 있음
    
    Emotion에는 다양한 사용방식이 있어
    1. style을 적용한 component 방식(styled component)
    2. component에 주입할 스타일 변수 선언 방식(css: emotion/react)
    을 샘플로 사용해 봄

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
+import { css } from '@emotion/react'
+import styled from '@emotion/styled'
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
+const bold = css`
+  font-weight: bold;
+`
+
+const containerStyles = css`
+  background-color: green;
+  ${bold}
+`
+
+const Button = styled.button`
+  width: 200px;
+  height: 100px;
+  color: blue;
+  ${bold}
+`
+
 function App() {
   return (
-    <div className="App">
+    <div className="App" css={containerStyles}>
+      <Button>스타일버튼</Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
