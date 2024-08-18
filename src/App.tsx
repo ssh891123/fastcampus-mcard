e6c4a9bb8261832e704069a0f4da4dcd8a5158d8
sunghyun_son <sunghyun_son@tmax.co.kr>
2024-08-18 11:14:45 +0900

    [feat] app 컴포넌트 페이지 구성
    - 1. app 컴포넌트의 불필요한 컴포넌트 제거
    - 2. 홈페이지와 테스트용(fire store에 mock data 업로드용) 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,15 @@
-import React from 'react'
-import './App.css'
-
-import Text from '@shared/Text'
-import Button from '@shared/Button'
-import Input from '@shared/Input'
-import TextField from '@shared/TextField'
-import { useAlertContext } from '@contexts/AlertContext'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePage from '@pages/Home'
+import TestPage from '@pages/Test'
 
 function App() {
-  const { open } = useAlertContext()
-
   return (
-    <div>
-      <Text typography="t1" display="block" color="red">
-        t1
-      </Text>
-      <Text typography="t2" color="green">
-        t2
-      </Text>
-      <Text typography="t3">t3</Text>
-      <Text typography="t4">t4</Text>
-      <Text typography="t5">t5</Text>
-      <Text typography="t6">t6</Text>
-      <Text typography="t7">t7</Text>
-
-      <div style={{ height: 10, width: '100%', background: '#efefef' }} />
-      <Button color="success"> 클릭해주세요 </Button>
-      <Button color="error"> 클릭해주세요 </Button>
-      <Button color="success" weak>
-        클릭해주세요
-      </Button>
-      <Button full> 클릭해주세요 </Button>
-      <Button disabled> 클릭해주세요 </Button>
-
-      <Input placeholder="로그인" aria-invalid={false} />
-      <Input aria-invalid={true} />
-
-      <TextField label="아이디" />
-      <TextField label="패스워드" hasError={true} />
-
-      <Button
-        onClick={() => {
-          open({
-            title: '카드 신청 완료',
-            description: '카드 신청이 완료되었습니다.',
-            onButtonClick: () => {},
-          })
-        }}
-      >
-        AlertOpen
-      </Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={HomePage} />
+        <Route path="/test" Component={TestPage} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Test.tsx
@@ -0,0 +1,6 @@
+// data를 fire store에 저장하기 위한 test 페이지
+function TestPage() {
+  return <div>TestPage</div>
+}
+
+export default TestPage
