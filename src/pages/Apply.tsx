085637fac169ef82ef176f1864548f49beba75de
sunghyun_son <sunghyun_son@tmax.co.kr>
2024-09-19 21:28:52 +0900

    [feat] 약관 페이지 구현
    - 약관 페이지의 약관 단위를 위한 Component 구현(Agreement.tsx)
      - title, description component으로 구현하였고, 사용처에서 조합해서 사용할 수 있도록 합성 컴포넌트로 구현
      - check 상태를 테두리 여부 및 선택 상태에 따라 스타일 변경하도록 구현
    
    - 약관 페이지 구현(Terms.tsx)
      - 약관 mock 데이터 추가: constants/apply.ts
      - 약관 데이터의 type 추가: models/apply.ts
      - 모든 약관 동의 기능, 모든 약관의 동의 여부 체크 기능 구현
      - 상위 페이지(Apply.tsx)에서 약관에 동의한 데이터 전달하도록 callback 호출
    
    - 카드 신청 페이지 기능 구현(Apply.tsx)
      - 약관 페이지에서 동의한 데이터 확인과 다음 페이지 이동을 위한 callback 함수를 prop으로 전달

--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -7,9 +7,13 @@ import CardInfo from '@components/apply/CardInfo'
 function ApplyPage() {
   const [step, setStep] = useState(0)
 
+  const handleTermsChange = (terms: string[]) => {
+    console.log('handleTermsChange', terms)
+  }
+
   return (
     <div>
-      {step === 0 ? <Terms /> : null}
+      {step === 0 ? <Terms onNext={handleTermsChange} /> : null}
       {step === 1 ? <BasicInfo /> : null}
       {step === 2 ? <CardInfo /> : null}
     </div>
